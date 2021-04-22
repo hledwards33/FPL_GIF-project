5ffee9a096d11e9618e0fdb093eda1697130ae30
BigTub <H.Edwards.2@outlook.com>
2021-04-22 08:59:56 +0100

    player code almost working

--- form/views.py
+++ form/views.py
@@ -2,24 +2,44 @@ from django.shortcuts import render
 import json
 import requests
 
-url = "https://fantasy.prmeierleague.com/api/bootstrap-static/"
 photo_url = "https://resources.premierleague.com/premierleague/photos/players/110x140/p{photo_num}.png"
 
+url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+
+
 def analysis(request):
-    return render(request, 'form/analysis.html')
+    player_data = get_player_data()
+    return render(request, 'form/analysis.html', {'player_data': player_data})
+
 
 def get_player_data():
     response = requests.get(url)
-    response = response.json()
+    response = json.loads(response.content)
     elements = response['elements']
 
-    d = {}
+    all_data = {}
     for i in range(len(elements)):
-        d[i] = elements[i]['form']
+        all_data[i] = elements[i]['form']
 
-    d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+    all_data = dict(sorted(all_data.items(), key=lambda item: item[1], reverse=True))
 
-    l=[]
+    player_dict = {}
     for i in range(11):
-        index = list(d.keys())[i]
-        l.append(elements[index])
+        index = list(all_data.keys())[i]
+        player_dict[i] = {'form': elements[index]['form'],
+                          'name': elements[index]['first_name'] + " " + elements[index]['second_name'],
+                          'news': elements[index]['news'],
+                          'dreamteam apps': elements[index]['dreamteam_count'],
+                          'selected by ': elements[index]['selected_by_percent'],
+                          'bp total': elements[index]['bonus'],
+                          'photo': elements[index]['photo'][:-4],
+                          'points': elements[index]['total_points'],
+                          'chance of playing': elements[index]['chance_of_playing_next_round'],
+                          'transfers in': elements[index]['transfers_in_event'],
+                          'transfers out': elements[index]['transfers_out_event'],
+                          'goals': elements[index]['goals_scored'],
+                          'assists': elements[index]['assists'],
+                          'clean sheets': elements[index]['clean_sheets'],
+                          'cost': elements[index]['now_cost'] / 10
+                          }
+        return player_dict
