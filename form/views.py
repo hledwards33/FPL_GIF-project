29ce8e207f11f3e75ee8f9cbd443b7b6a614ea30
BigTub <H.Edwards.2@outlook.com>
2021-04-22 10:59:53 +0100

    player form page finished

--- form/views.py
+++ form/views.py
@@ -24,23 +24,24 @@ def get_player_data():
     all_data = dict(sorted(all_data.items(), key=lambda item: item[1], reverse=True))
 
     player_dict = {}
-    for i in range(11):
+    for i in range(100):
         index = list(all_data.keys())[i]
         player_dict[i] = {
-                          'form': elements[index]['form'],
-                          'name': elements[index]['first_name'] + " " + elements[index]['second_name'],
-                          'news': elements[index]['news'],
-                          'dreamteam apps': elements[index]['dreamteam_count'],
-                          'selected by ': elements[index]['selected_by_percent'],
-                          'bp total': elements[index]['bonus'],
-                          'photo': elements[index]['photo'][:-4],
-                          'points': elements[index]['total_points'],
-                          'chance of playing': elements[index]['chance_of_playing_next_round'],
-                          'transfers in': elements[index]['transfers_in_event'],
-                          'transfers out': elements[index]['transfers_out_event'],
-                          'goals': elements[index]['goals_scored'],
-                          'assists': elements[index]['assists'],
-                          'clean sheets': elements[index]['clean_sheets'],
-                          'cost': elements[index]['now_cost'] / 10
+                          'form':elements[index]['form'],
+                          'name':elements[index]['first_name'] + " " + elements[index]['second_name'],
+                          'news':elements[index]['news'],
+                          'dreamteam_apps':elements[index]['dreamteam_count'],
+                          'selected_by':elements[index]['selected_by_percent'],
+                          'bp_total':elements[index]['bonus'],
+                          'photo':photo_url.format(photo_num=elements[index]['photo'][:-4]),
+                          'points':elements[index]['total_points'],
+                          'chance_of_playing':elements[index]['chance_of_playing_next_round'],
+                          'transfers_in':elements[index]['transfers_in_event'],
+                          'transfers_out':elements[index]['transfers_out_event'],
+                          'goals':elements[index]['goals_scored'],
+                          'assists':elements[index]['assists'],
+                          'clean_sheets':elements[index]['clean_sheets'],
+                          'cost':elements[index]['now_cost'] / 10,
+                          'code':elements[index]['code']
                           }
     return player_dict
