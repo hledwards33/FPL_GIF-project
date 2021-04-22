2727009381c0741caaf45c5eeac87830cf12c4c0
BigTub <H.Edwards.2@outlook.com>
2021-04-22 11:29:58 +0100

    added graph results page

--- charts/urls.py
+++ charts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('graphs', views.graphs, name='graphs'),
+    path('graphs_result', views.graphs_result, name='graphs_result'),
 ]
--- charts/views.py
+++ charts/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def graphs(request):
     return render(request, "charts/graphs.html")
+
+def graphs_result(request):
+    return render(request, "charts/graphs_result.html")
